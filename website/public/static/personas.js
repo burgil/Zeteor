00d6f62e2fc221df69a4dbe7cf2c62628bfc6d92
Burgil <41600149+burgil@users.noreply.github.com>
2024-05-05 12:51:16 +0300

    Added personas previews

--- website/public/static/personas.js
+++ website/public/static/personas.js
@@ -10,12 +10,12 @@ document.getElementById('persona-0').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -38,12 +38,12 @@ document.getElementById('persona-1').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -66,12 +66,12 @@ document.getElementById('persona-2').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -94,12 +94,12 @@ document.getElementById('persona-3').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -122,12 +122,12 @@ document.getElementById('persona-4').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -150,12 +150,12 @@ document.getElementById('persona-5').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -178,12 +178,12 @@ document.getElementById('persona-6').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -206,12 +206,12 @@ document.getElementById('persona-7').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
@@ -234,12 +234,12 @@ document.getElementById('persona-8').addEventListener('click', function () {
                     class: 'popup-person-preview',
                     elements: [
                         {
-                            type: 'span',
+                            type: 'h1',
                             txt: personaName,
                         },
                         {
-                            type: 'span',
-                            txt: 'test',
+                            type: 'div',
+                            html: `<iframe width="100%" height="630" src="https://www.youtube-nocookie.com/embed/UwsrzCVZAb8?si=1sQJTgTD0rMcM0bw&controls=1&autoplay=1&rel=0&modestbranding=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
                         },
                     ]
                 }
