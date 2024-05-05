76e1964c0bf30dd75278bacacb25b2e1acc7043c
Burgil <41600149+burgil@users.noreply.github.com>
2024-05-05 12:41:25 +0300

    Added AI Persona Popups

---
+++ website/public/static/personas.js
@@ -0,0 +1,252 @@
+document.getElementById('persona-0').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-1').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-2').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-3').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-4').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-5').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-6').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-7').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
+document.getElementById('persona-8').addEventListener('click', function () {
+    const personaName = this.parentElement.querySelector('.song-title h2').textContent;
+    modalBuilder(document.getElementById('modal-content'), [
+        {
+            type: 'div',
+            class: 'popup-persona-preview-container',
+            elements: [
+                {
+                    type: 'div',
+                    class: 'popup-person-preview',
+                    elements: [
+                        {
+                            type: 'span',
+                            txt: personaName,
+                        },
+                        {
+                            type: 'span',
+                            txt: 'test',
+                        },
+                    ]
+                }
+            ]
+        },
+    ]);
+    requestAnimationFrame(function () {
+        openModal('modal-popup');
+    });
+});
