6cca3f53a1a29f952465fa9d99ca67a701950ffa
Burgil <41600149+burgil@users.noreply.github.com>
2024-05-07 04:52:47 +0300

    Update website_update.sh

--- website/website_update.sh
+++ website/website_update.sh
@@ -4,6 +4,8 @@ OWNER="burgil"
 REPO_NAME="Zeteor"
 
 rm nohup.out
+ps aux | grep "ZeteorMainWebsiteUpdater" | grep -v grep | awk '{print $2}' | xargs -I{} kill -9 {}
+
 get_latest_commit() {
     curl -s "https://api.github.com/repos/$OWNER/$REPO_NAME/commits/main" | grep '"sha"' | head -n 1 | cut -d '"' -f 4
 }
@@ -12,22 +14,26 @@ while true; do
     LATEST_COMMIT=$(get_latest_commit)
     echo "LATEST COMMIT: $LATEST_COMMIT"
     if [ "$LATEST_COMMIT" != "$LAST_COMMIT" ]; then
-        echo "Changes detected in the repository!"
-        LAST_COMMIT=$LATEST_COMMIT
-        echo "Restarting Website..."
-        ps aux | grep "ZeteorMainWebsite" | grep -v grep | awk '{print $2}' | xargs -I{} kill -9 {}
-        cd ..
-        echo "Fetching Updates..."
-        git fetch
-        echo "Pulling Updates..."
-        git pull
-        cd website
-        echo "Installing Website..."
-        npm install
-        rm nohup.out
-        echo "Starting Website..."
-        nohup npm start &
-        echo "Website Restarted!"
+        if [ "$LATEST_COMMIT" != "" ]; then
+            if [ "" != "$LAST_COMMIT" ]; then
+                echo "Changes detected in the repository!"
+                LAST_COMMIT=$LATEST_COMMIT
+                echo "Restarting Website..."
+                ps aux | grep "ZeteorMainWebsite" | grep -v grep | awk '{print $2}' | xargs -I{} kill -9 {}
+                cd ..
+                echo "Fetching Updates..."
+                git fetch
+                echo "Pulling Updates..."
+                git pull
+                cd website
+                echo "Installing Website..."
+                npm install
+                rm nohup.out
+                echo "Starting Website..."
+                nohup npm start &
+                echo "Website Restarted!"
+            fi
+        fi
     fi
-    sleep 120
+    sleep 140
 done
